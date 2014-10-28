cd48ebdeb177661e7af934b46a314a9b32db2a51
Carol Gomes <carol.gomes.vargas@gmail.com>
2014-10-28 17:32:32 -0200

    Criação das telas inicial e protótipo da tela de configuração do jogo - transições entre telas funcionado

--- src/br/uff/es2/war/view/Teste.java
+++ src/br/uff/es2/war/view/Teste.java
@@ -37,7 +37,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
-import org.eclipse.persistence.exceptions.JAXBException;
+//import org.eclipse.persistence.exceptions.JAXBException;
 
 /**
  *
