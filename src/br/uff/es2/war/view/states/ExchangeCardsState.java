85d9eaa7c88adcd72f599b29f77a5dbc054d899a
Baral <Baral@10.20.15.145>
2014-11-13 10:56:52 -0200

    ExchangeCardsState

--- src/br/uff/es2/war/view/states/ExchangeCardsState.java
+++ src/br/uff/es2/war/view/states/ExchangeCardsState.java
@@ -1,13 +1,26 @@
 package br.uff.es2.war.view.states;
 
+import br.uff.es2.war.model.Card;
+import br.uff.es2.war.model.phases.ReceiveSoldiersPhase;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 import br.uff.es2.war.view.GameController;
+import br.uff.es2.war.view.GameController2;
+import br.uff.es2.war.view.PopUpController;
+import java.util.List;
 
 public class ExchangeCardsState implements ViewState {
 
     @Override
     public void execute(GameController controller) {
-	throw new NotImplementedException();
+        PopUpController pc = controller.getGameController2().getPopUpController();
+        List<Card> cartas = pc.getCartasTroca();
+        
+        if(ReceiveSoldiersPhase.checkExchange(cartas)){
+            pc.atualizaCartas();
+            pc.mostraCartas();
+        }
+        
+        
     }
 
 }
