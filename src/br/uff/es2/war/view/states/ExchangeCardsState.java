b1665af23bd0c34453d2dae7f62844897a1225cb
Baral <Baral@10.20.15.145>
2014-11-13 11:11:38 -0200

    ExchangeCardsState

--- src/br/uff/es2/war/view/states/ExchangeCardsState.java
+++ src/br/uff/es2/war/view/states/ExchangeCardsState.java
@@ -2,10 +2,12 @@ package br.uff.es2.war.view.states;
 
 import br.uff.es2.war.model.Card;
 import br.uff.es2.war.model.phases.ReceiveSoldiersPhase;
+import static br.uff.es2.war.model.phases.ReceiveSoldiersPhase.checkExchange;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 import br.uff.es2.war.view.GameController;
 import br.uff.es2.war.view.GameController2;
 import br.uff.es2.war.view.PopUpController;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ExchangeCardsState implements ViewState {
@@ -14,13 +16,21 @@ public class ExchangeCardsState implements ViewState {
     public void execute(GameController controller) {
         PopUpController pc = controller.getGameController2().getPopUpController();
         List<Card> cartas = pc.getCartasTroca();
-        
-        if(ReceiveSoldiersPhase.checkExchange(cartas)){
+
+        if (ReceiveSoldiersPhase.checkExchange(pc.getCartas())) {
+            // é possível fazer trocas
             pc.atualizaCartas();
             pc.mostraCartas();
+            if (pc.getCartas().size() == 5) {
+                //obrigatório trocar
+                pc.bloqueiaParaTroca();
+            } else {
+                //Pode realizar trocas
+                pc.trocarCartas();
+            }
+
         }
-        
-        
+
     }
 
 }
