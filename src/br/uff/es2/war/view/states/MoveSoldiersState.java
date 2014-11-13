36c3d265c1a5bddbd4a1c7fba6248feb9db89e20
Arthur Pitzer <arthurpitzer@id.uff.br>
2014-11-13 09:24:55 -0200

    state faltando

---
+++ src/br/uff/es2/war/view/states/MoveSoldiersState.java
@@ -0,0 +1,13 @@
+package br.uff.es2.war.view.states;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import br.uff.es2.war.view.GameController;
+
+public class MoveSoldiersState implements ViewState {
+
+    @Override
+    public void execute(GameController controller) {
+	throw new NotImplementedException();
+    }
+
+}
