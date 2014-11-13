35565977b6450712312ef2d62c5c3775e66bf130
Carol Gomes <carol.gomes.vargas@gmail.com>
2014-11-13 17:48:27 -0200

    Inicializando botão continua

--- src/br/uff/es2/war/view/GameController.java
+++ src/br/uff/es2/war/view/GameController.java
@@ -327,7 +327,8 @@ public class GameController implements Initializable {
 		    @Override
 		    public void onAction(AnswerCombatEvent args) {
 			System.out.println("Client AnswerCombat");
-			ViewState state = new AnswerState();
+			AnswerState state = new AnswerState();
+                        state.setBtn(btn_continua);
 			state.execute(GameController.this);
 		    }
 		});
